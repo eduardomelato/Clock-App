2e81aa18a9678982216a6cc29040660b10bbd928
Eduardo Melato <edumelato@gmail.com>
2022-01-14 11:13:47 -0300

    "done"

--- clock_app/test/widget_test.dart
+++ clock_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:clock_app/main.dart';
+import 'package:analog_clock/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
